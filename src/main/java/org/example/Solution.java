a1879f299961d04860160a9c5219af8bb99ed280
João Paulo <joao9aulo@gmail.com>
2025-05-08 08:46:08 -0300

    exercicio

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 class Solution {
 
     public static void main(String[] args) {
-        int[] nums1 = {-1,0,1,1,0,0,0,0,0};
-        int[] nums2 = {-1,0,2,2,3};
-        merge(nums1, 4, nums2, 5);
+        int[] nums1 = {-12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        int[] nums2 = {-49,-45,-42,-41,-40,-39,-39,-39,-38,-36,-34,-34,-33,-33,-32,-31,-29,-28,-26,-26,-24,-21,-20,-20,-18,-16,-16,-14,-11,-7,-6,-5,-4,-4,-3,-3,-2,-2,-1,0,0,0,2,2,6,7,7,8,10,10,13,13,15,15,16,17,17,19,19,20,20,20,21,21,22,22,24,24,25,26,27,29,30,30,30,35,36,36,36,37,39,40,41,42,45,46,46,46,47,48};
+        merge(nums1, 1, nums2,90);
         System.out.println(Arrays.toString(nums1));
     }
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -46,9 +46,21 @@ class Solution {
         if(anterior<0){
             return false;
         }
-        int posterior = i+1;
-        boolean temPosterior = posterior<nums1.length && nums1[posterior]!=0;
+        boolean temPosterior = temPosterior(nums1,i+1);
         return (nums1[anterior] >= nums1[i] && nums1[i] == 0 && !temPosterior);
+    }
+
+    private static boolean temPosterior(int[] nums1, int i) {
+        if(i>nums1.length){
+            return false;
+        }
+       for(int k=i;k<nums1.length;k++){
+           if(nums1[k]>0){
+               return true;
+           }
+       }
+
+       return false;
     }
 
     private static void andarUm(int[] nums1, int valor, int i) {
